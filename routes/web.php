9d2c6b1a7004dca0190d2b3366e33476576bcb1e
Kaung_Myat_Thway <kaung.myatthwe@seattleconsultingmyanmar.com>
2022-08-11 20:09:56 +0630

    Logout User

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,14 @@
 <?php
 
-use App\Http\Controllers\Auth\RegisterController;
-use App\Http\Controllers\DashboardController;
-use App\Http\Controllers\LoginController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\LogoutController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\Auth\RegisterController;
+
+Route::get('/', function() {
+    return view('home');
+})->name('home');
 
 Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 
@@ -12,6 +17,8 @@ Route::post('/register', [RegisterController::class, 'store']);
 
 Route::get('/login', [LoginController::class, 'index'])->name('login');
 Route::post('/login', [LoginController::class, 'store']);
+
+Route::post('/logout', [LogoutController::class, 'store'])->name('logout');
 
 Route::get('/posts', function () {
     return view('posts.index');
