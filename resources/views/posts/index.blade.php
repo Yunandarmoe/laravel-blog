a57440ffecf62e6192cfc0a40cea1e2289a8d569
Kaung_Myat_Thway <kaung.myatthwe@seattleconsultingmyanmar.com>
2022-08-12 21:17:38 +0630

    Pagination

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -30,6 +30,8 @@
                         <p class="mb-2">{{ $post->body }}</p>
                     </div>
                 @endforeach
+
+                {{ $posts->links() }}
             @else
                 <p>No Post</p>
             @endif
